6a8361238f98c0a99a86b7033433598e8e2f612d
Shudi Wang <shudiwsh2009@gmail.com>
2015-11-21 14:23:57 +0800

    source profile in deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,6 +6,7 @@ go get -u github.com/shudiwsh2009/reservation_thxx_go_reminder
 echo "#deploy website"
 supervisorctl stop reservation_thxx_go
 sleep 5
+source /etc/profile
 cd $GOPATH/src/github.com/shudiwsh2009/reservation_thxx_go
 go build
 supervisorctl start reservation_thxx_go
