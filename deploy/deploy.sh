a19694cf7d5af068129a7048b701d511ab3af0e5
Shudi Wang <shudiwsh2009@gmail.com>
2016-09-07 23:00:02 +0800

    FIX: log dir

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -11,12 +11,12 @@ go install github.com/shudiwsh2009/reservation_thxx_go/server
 kill -9 $(lsof -t -i:8080)
 sleep 5
 now=$(date +"%Y_%m_%d_%T")
-mv ~/thxxfzzx_go/server.log ~/thxxfzzx_go/server-${now}.log
+mv ~/thxxfzzx_go/log/server.log ~/thxxfzzx_go/log/server-${now}.log
 cp -f $GOPATH/bin/server ./server.run
 cp -rf $GOPATH/src/github.com/shudiwsh2009/reservation_thxx_go/templates ./templates
 cp -rf $GOPATH/src/github.com/shudiwsh2009/reservation_thxx_go/assets ./assets
 chmod a+x ./server.run
-nohup ./server.run --app-env="ONLINE" --sms-uid="shudiwsh2009" --sms-key="946fee2e7ad699b065f1" > server.log 2>&1 & echo $! > ~/thxxfzzx_go/run.pid &
+nohup ./server.run --app-env="ONLINE" --sms-uid="shudiwsh2009" --sms-key="946fee2e7ad699b065f1" > log/server.log 2>&1 & echo $! > ~/thxxfzzx_go/run.pid &
 
 echo "#deploy reminder"
 cd ~/thxxfzzx_go
