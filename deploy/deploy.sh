43f940e0c9b3f067e41c85eb76925eca1122b536
Shudi Wang <shudiwsh2009@gmail.com>
2016-09-07 22:55:14 +0800

    FIX: deploy reminder

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -12,15 +12,16 @@ kill -9 $(lsof -t -i:8080)
 sleep 5
 now=$(date +"%Y_%m_%d_%T")
 mv ~/thxxfzzx_go/server.log ~/thxxfzzx_go/server-${now}.log
-cp $GOPATH/bin/server ./server.run
-cp -r $GOPATH/src/github.com/shudiwsh2009/reservation_thxx_go/templates ./templates
-cp -r $GOPATH/src/github.com/shudiwsh2009/reservation_thxx_go/assets ./assets
+cp -f $GOPATH/bin/server ./server.run
+cp -rf $GOPATH/src/github.com/shudiwsh2009/reservation_thxx_go/templates ./templates
+cp -rf $GOPATH/src/github.com/shudiwsh2009/reservation_thxx_go/assets ./assets
 chmod a+x ./server.run
 nohup ./server.run --app-env="ONLINE" --sms-uid="shudiwsh2009" --sms-key="946fee2e7ad699b065f1" > server.log 2>&1 & echo $! > ~/thxxfzzx_go/run.pid &
 
 echo "#deploy reminder"
 cd ~/thxxfzzx_go
-go build -o ./reminder.run $GOPATH/src/github.com/shudiwsh2009/reservation_thxx_go/reminder
+go install github.com/shudiwsh2009/reservation_thxx_go/reminder
+cp -f $GOPATH/bin/reminder ./reminder.run
 chmod a+x ./reminder.run
 #0 20 * * * ~/thxxfzzx_go/reminder.run --app-env="ONLINE" --sms-uid="shudiwsh2009" --sms-key="946fee2e7ad699b065f1"
 echo "restart cron"
