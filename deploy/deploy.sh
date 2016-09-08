5b90931427935775a8dcb6ccfd185ce7ea56fc0a
Shudi Wang <shudiwsh2009@gmail.com>
2016-09-08 21:00:13 +0800

    FIX: deploy dir bug

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -15,15 +15,16 @@ mv ~/thxxfzzx_go/log/server.log ~/thxxfzzx_go/log/server-${now}.log
 cp -f $GOPATH/bin/server ./server/server.run
 cp -rf $GOPATH/src/github.com/shudiwsh2009/reservation_thxx_go/templates ./templates
 cp -rf $GOPATH/src/github.com/shudiwsh2009/reservation_thxx_go/assets ./assets
-chmod a+x ./server/server.run
-nohup ./server/server.run --app-env="ONLINE" --sms-uid="shudiwsh2009" --sms-key="946fee2e7ad699b065f1" > log/server.log 2>&1 & echo $! > ~/thxxfzzx_go/run.pid &
+cd ~/thxxfzzx_go/server
+chmod a+x ./server.run
+nohup ./server.run --app-env="ONLINE" --sms-uid="shudiwsh2009" --sms-key="946fee2e7ad699b065f1" > ~/thxxfzzx_go/log/server.log 2>&1 & echo $! > ~/thxxfzzx_go/run.pid &
 
 echo "#deploy reminder"
 cd ~/thxxfzzx_go
 go install github.com/shudiwsh2009/reservation_thxx_go/reminder
 cp -f $GOPATH/bin/reminder ./server/reminder.run
 chmod a+x ./server/reminder.run
-#0 20 * * * ~/thxxfzzx_go/server/reminder.run --app-env="ONLINE" --sms-uid="shudiwsh2009" --sms-key="946fee2e7ad699b065f1"
+#0 20 * * * /root/thxxfzzx_go/server/reminder.run --app-env="ONLINE" --sms-uid="shudiwsh2009" --sms-key="946fee2e7ad699b065f1"
 echo "restart cron"
 service cron restart
 
