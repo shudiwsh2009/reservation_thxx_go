e3c21311cdee39fb2e6d476fee246b085b2d3786
Shudi Wang <wangsh01@fenbi.com>
2021-11-05 12:16:05 +0800

    FIX: export excel using new thrid-party tool

--- buslogic/export.go
+++ buslogic/export.go
@@ -6,120 +6,77 @@ import (
 	"github.com/shudiwsh2009/reservation_thxx_go/model"
 	re "github.com/shudiwsh2009/reservation_thxx_go/rerror"
 	"github.com/shudiwsh2009/reservation_thxx_go/utils"
-	"github.com/tealeg/xlsx"
+	"github.com/xuri/excelize"
 	"path/filepath"
 	"sort"
 	"time"
 )
 
-// 定义单元格样式
-var (
-	textCenterAlignment = xlsx.Alignment{
-		Horizontal: "center",
-		Vertical:   "center",
-	}
-	textTopAlignment = xlsx.Alignment{
-		Horizontal: "left",
-		Vertical:   "up",
-	}
-	border = xlsx.Border{
-		Left:        "thin",
-		LeftColor:   "000000",
-		Right:       "thin",
-		RightColor:  "000000",
-		Top:         "thin",
-		TopColor:    "000000",
-		Bottom:      "thin",
-		BottomColor: "000000",
-	}
+const ExportFileDefaultSheetName string = "export"
 
-	borderStyle *xlsx.Style = &xlsx.Style{
-		ApplyBorder: true,
-		Border:      border,
-	}
-	textCenterStyle *xlsx.Style = &xlsx.Style{
-		ApplyAlignment: true,
-		ApplyBorder:    true,
-		Alignment:      textCenterAlignment,
-		Border:         border,
-	}
-	textTopStyle *xlsx.Style = &xlsx.Style{
-		ApplyAlignment: true,
-		ApplyBorder:    true,
-		Alignment:      textTopAlignment,
-		Border:         border,
-	}
-)
+var StudentFeedbackChoice = map[uint8]string{
+	'A': "非常同意",
+	'B': "一般",
+	'C': "不同意",
+}
+
+var ReservationRoomColumn = map[int][]string{
+	1:  {"B", "C"},
+	2:  {"D", "E"},
+	3:  {"F", "G"},
+	4:  {"H", "I"},
+	5:  {"J", "K"},
+	6:  {"L", "M"},
+	7:  {"N", "O"},
+	8:  {"P", "Q"},
+	9:  {"R", "S"},
+	10: {"T", "U"},
+	11: {"V", "W"},
+	12: {"X", "Y"},
+}
 
 func (w *Workflow) ExportReservationsToFile(reservations []*model.Reservation, path string) error {
-	var file *xlsx.File
-	var sheet *xlsx.Sheet
-	var row *xlsx.Row
-	var cell *xlsx.Cell
 	var err error
-	file, err = xlsx.OpenFile(filepath.Join(utils.ExportFolder, "export_template.xlsx"))
+	file, err := excelize.OpenFile(filepath.Join(utils.ExportFolder, "export_template.xlsx"))
 	if err != nil {
 		return re.NewRError("fail to oepn export template", err)
 	}
-	sheet = file.Sheet["export"]
-	if sheet == nil {
-		return re.NewRError("fail to open sheet", err)
-	}
-	for _, res := range reservations {
-		row = sheet.AddRow()
+	for i, res := range reservations {
+		rowNum := i + 2
 		// 学生申请表
-		cell = row.AddCell()
-		cell.SetString(res.StudentInfo.Fullname)
-		cell = row.AddCell()
-		cell.SetString(res.StudentInfo.Gender)
-		cell = row.AddCell()
-		cell.SetString(res.StudentInfo.Username)
-		cell = row.AddCell()
-		cell = row.AddCell()
-		cell.SetString(res.StudentInfo.School)
-		cell = row.AddCell()
-		cell.SetString(res.StudentInfo.Hometown)
-		cell = row.AddCell()
-		cell.SetString(res.StudentInfo.Mobile)
-		cell = row.AddCell()
-		cell.SetString(res.StudentInfo.Email)
-		cell = row.AddCell()
-		cell = row.AddCell()
-		cell.SetString(res.StudentInfo.Problem)
+		file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("A%d", rowNum), res.StudentInfo.Fullname)
+		file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("B%d", rowNum), res.StudentInfo.Gender)
+		file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("C%d", rowNum), res.StudentInfo.Username)
+		file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("E%d", rowNum), res.StudentInfo.School)
+		file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("F%d", rowNum), res.StudentInfo.Hometown)
+		file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("G%d", rowNum), res.StudentInfo.Mobile)
+		file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("H%d", rowNum), res.StudentInfo.Email)
+		file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("J%d", rowNum), res.StudentInfo.Problem)
 		// 预约信息
-		cell = row.AddCell()
-		cell.SetString(res.TeacherFullname)
-		cell = row.AddCell()
-		cell.SetString(res.StartTime.Format("2006-01-02"))
+		file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("K%d", rowNum), res.TeacherFullname)
+		file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("L%d", rowNum), res.StartTime.Format("2006-01-02"))
 		// 咨询师反馈表
-		cell = row.AddCell()
-		cell = row.AddCell()
-		cell = row.AddCell()
-		cell.SetString(res.TeacherFeedback.Problem)
-		cell = row.AddCell()
-		cell.SetString(res.TeacherFeedback.Solution)
+		file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("O%d", rowNum), res.TeacherFeedback.Problem)
+		file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("P%d", rowNum), res.TeacherFeedback.Solution)
 		// 学生反馈表
-		cell = row.AddCell()
-		cell = row.AddCell()
-		cell.SetString(res.StudentFeedback.Score)
-		cell = row.AddCell()
-		cell.SetString(res.StudentFeedback.Feedback)
-		if res.StudentFeedback.Choices != "" {
-			for i := 0; i < len(res.StudentFeedback.Choices); i++ {
-				cell = row.AddCell()
-				switch res.StudentFeedback.Choices[i] {
-				case 'A':
-					cell.SetString("非常同意")
-				case 'B':
-					cell.SetString("一般")
-				case 'C':
-					cell.SetString("不同意")
-				default:
-				}
-			}
+		file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("R%d", rowNum), res.StudentFeedback.Score)
+		file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("S%d", rowNum), res.StudentFeedback.Feedback)
+		if res.StudentFeedback.Choices != "" && len(res.StudentFeedback.Choices) == 12 {
+			file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("T%d", rowNum), StudentFeedbackChoice[res.StudentFeedback.Choices[0]])
+			file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("U%d", rowNum), StudentFeedbackChoice[res.StudentFeedback.Choices[1]])
+			file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("V%d", rowNum), StudentFeedbackChoice[res.StudentFeedback.Choices[2]])
+			file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("W%d", rowNum), StudentFeedbackChoice[res.StudentFeedback.Choices[3]])
+			file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("X%d", rowNum), StudentFeedbackChoice[res.StudentFeedback.Choices[4]])
+			file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("Y%d", rowNum), StudentFeedbackChoice[res.StudentFeedback.Choices[5]])
+			file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("Z%d", rowNum), StudentFeedbackChoice[res.StudentFeedback.Choices[6]])
+			file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("AA%d", rowNum), StudentFeedbackChoice[res.StudentFeedback.Choices[7]])
+			file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("AB%d", rowNum), StudentFeedbackChoice[res.StudentFeedback.Choices[8]])
+			file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("AC%d", rowNum), StudentFeedbackChoice[res.StudentFeedback.Choices[9]])
+			file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("AD%d", rowNum), StudentFeedbackChoice[res.StudentFeedback.Choices[10]])
+			file.SetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("AE%d", rowNum), StudentFeedbackChoice[res.StudentFeedback.Choices[11]])
 		}
 	}
-	err = file.Save(path)
+	err = file.SaveAs(path)
 	if err != nil {
 		return re.NewRError("fail to save to file", err)
 	}
@@ -190,115 +147,192 @@ func (w *Workflow) ExportReservationArrangementsToFile(reservations []*model.Res
 	}
 
 	// 开始写入文件
-	var file *xlsx.File
-	var sheet *xlsx.Sheet
-	var row *xlsx.Row
-	var cell *xlsx.Cell
 	var err error
-	xlsx.SetDefaultFont(15, "宋体")
-	file = xlsx.NewFile()
-	// 咨询师排班表
-	sheet, err = file.AddSheet("咨询师排班表")
+	file := excelize.NewFile()
+	file.SetDefaultFont("宋体")
+	// 创建样式
+	// 边框
+	borderStyle, err := file.NewStyle(`{
+		"border": [
+		{
+			"type": "left",
+			"color": "000000",
+			"style": 1
+		},
+		{
+			"type": "top",
+			"color": "000000",
+			"style": 1
+		},
+		{
+			"type": "bottom",
+			"color": "000000",
+			"style": 1
+		},
+		{
+			"type": "right",
+			"color": "000000",
+			"style": 1
+		}]
+	}`)
 	if err != nil {
-		return re.NewRError("fail to add sheet for arrangement", err)
+		return re.NewRError("failed to create border style", err)
 	}
+	// 文字居中带边框
+	textCenterStyle, err := file.NewStyle(`{
+		"alignment": {
+			"horizontal": "center",
+			"vertical": "center"
+		},
+		"border": [
+			{
+				"type": "left",
+				"color": "000000",
+				"style": 1
+			},
+			{
+				"type": "top",
+				"color": "000000",
+				"style": 1
+			},
+			{
+				"type": "bottom",
+				"color": "000000",
+				"style": 1
+			},
+			{
+				"type": "right",
+				"color": "000000",
+				"style": 1
+			}
+		]
+	}`)
+	// 文字左上带边框
+	textLeftTopStyle, err := file.NewStyle(`{
+		"alignment": {
+			"horizontal": "left",
+			"vertical": "up"
+		},
+		"border": [
+			{
+				"type": "left",
+				"color": "000000",
+				"style": 1
+			},
+			{
+				"type": "top",
+				"color": "000000",
+				"style": 1
+			},
+			{
+				"type": "bottom",
+				"color": "000000",
+				"style": 1
+			},
+			{
+				"type": "right",
+				"color": "000000",
+				"style": 1
+			}
+		]
+	}`)
+
+	// 咨询师排班表
+	sheetIndex := file.NewSheet(ExportFileDefaultSheetName)
+	file.SetActiveSheet(sheetIndex)
 	// 第一表头
-	row = sheet.AddRow()
-	cell = row.AddCell()
-	cell.SetStyle(borderStyle)
-	cell.SetValue("时间")
+	file.SetCellValue(ExportFileDefaultSheetName, "A1", "时间")
+	file.SetCellStyle(ExportFileDefaultSheetName, "A1", "A1", borderStyle)
+	if len(arrangement.Rooms) > len(ReservationRoomColumn) {
+		return re.NewRError("not enough reservation rooms", nil)
+	}
 	for i := 1; i <= len(arrangement.Rooms); i++ {
-		cell = row.AddCell()
-		cell.SetStyle(borderStyle)
-		cell.SetValue(fmt.Sprintf("咨询室%d", i))
-		cell = row.AddCell()
-		cell.SetStyle(borderStyle)
+		cellIndex := fmt.Sprintf("%s1", ReservationRoomColumn[i][0])
+		file.SetCellValue(ExportFileDefaultSheetName, cellIndex, fmt.Sprintf("咨询室%d", i))
+		file.SetCellStyle(ExportFileDefaultSheetName, cellIndex, cellIndex, borderStyle)
 	}
 	// 合并第一表头
-	for i := 0; i < len(arrangement.Rooms); i++ {
-		cell = row.Cells[2*i+1]
-		cell.Merge(1, 0)
-		cell.SetStyle(textCenterStyle)
+	for i := 1; i <= len(arrangement.Rooms); i++ {
+		firstCellIndex := fmt.Sprintf("%s1", ReservationRoomColumn[i][0])
+		secondCellIndex := fmt.Sprintf("%s1", ReservationRoomColumn[i][1])
+		file.MergeCell(ExportFileDefaultSheetName, firstCellIndex, secondCellIndex)
+		file.SetCellStyle(ExportFileDefaultSheetName, firstCellIndex, secondCellIndex, textCenterStyle)
 	}
 	// 第二表头
-	row = sheet.AddRow()
-	cell = row.AddCell()
-	cell.SetStyle(borderStyle)
-	for i := 0; i < len(arrangement.Rooms); i++ {
-		cell = row.AddCell()
-		cell.SetStyle(borderStyle)
-		cell.SetValue("咨询师")
-		cell = row.AddCell()
-		cell.SetStyle(borderStyle)
-		cell.SetValue("学生")
+	for i := 1; i <= len(arrangement.Rooms); i++ {
+		teacherCellIndex := fmt.Sprintf("%s2", ReservationRoomColumn[i][0])
+		studentCellIndex := fmt.Sprintf("%s2", ReservationRoomColumn[i][1])
+		file.SetCellValue(ExportFileDefaultSheetName, teacherCellIndex, "咨询师")
+		file.SetCellStyle(ExportFileDefaultSheetName, teacherCellIndex, teacherCellIndex, borderStyle)
+		file.SetCellValue(ExportFileDefaultSheetName, studentCellIndex, "学生")
+		file.SetCellStyle(ExportFileDefaultSheetName, studentCellIndex, studentCellIndex, borderStyle)
 	}
 	// 合并第一列头
-	row = sheet.Rows[0]
-	cell = row.Cells[0]
-	cell.Merge(0, 1)
-	cell.SetStyle(textCenterStyle)
+	file.MergeCell(ExportFileDefaultSheetName, "A1", "A2")
+	file.SetCellStyle(ExportFileDefaultSheetName, "A1", "A1", textCenterStyle)
 	// 填充咨询ID数据
 	start := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), 8, 0, 0, 0, time.Local)
 	end := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), 18, 0, 0, 0, time.Local)
-	for ts := start; ts.Before(end); ts = ts.Add(30 * time.Minute) {
-		row = sheet.AddRow()
-		cell = row.AddCell()
-		cell.SetStyle(textCenterStyle)
-		cell.SetValue(fmt.Sprintf("%s-%s", ts.Format("15:04"), ts.Add(30 * time.Minute).Format("15:04")))
-		for _, room := range arrangement.Rooms {
+	for ts, rowIndex := start, 3; ts.Before(end); ts, rowIndex = ts.Add(30*time.Minute), rowIndex+1 {
+		// 第一列的时间区间
+		timeCellIndex := fmt.Sprintf("A%d", rowIndex)
+		file.SetCellValue(ExportFileDefaultSheetName, timeCellIndex,
+			fmt.Sprintf("%s-%s", ts.Format("15:04"), ts.Add(30*time.Minute).Format("15:04")))
+		file.SetCellStyle(ExportFileDefaultSheetName, timeCellIndex, timeCellIndex, textCenterStyle)
+		for i, room := range arrangement.Rooms {
 			if r, ok := room.Timetable[ts.Format("15:04")]; ok {
-				cell = row.AddCell()
-				cell.SetStyle(borderStyle)
-				cell.SetValue(r.Id.Hex())
-				cell = row.AddCell()
-				cell.SetStyle(borderStyle)
-			} else {
-				cell = row.AddCell()
-				cell.SetStyle(borderStyle)
-				cell = row.AddCell()
-				cell.SetStyle(borderStyle)
+				arrangementCellIndex := fmt.Sprintf("%s%d", ReservationRoomColumn[i+1][0], rowIndex)
+				file.SetCellValue(ExportFileDefaultSheetName, arrangementCellIndex, r.Id.Hex())
+				file.SetCellStyle(ExportFileDefaultSheetName, arrangementCellIndex, arrangementCellIndex, borderStyle)
 			}
 		}
 	}
 	// 合并咨询并填充详细信息
 	timeRowNum := int(end.Sub(start) / (30 * time.Minute))
 	for i := range arrangement.Rooms {
-		for j := 2; j < timeRowNum+2; {
-			firstTeacherCell := sheet.Cell(j, 2*i+1)
-			if firstTeacherCell.Value != "" {
+		for j := 3; j <= timeRowNum+2; {
+			firstTeacherCellIndex := fmt.Sprintf("%s%d", ReservationRoomColumn[i+1][0], j)
+			firstTeacherCellValue, _ := file.GetCellValue(ExportFileDefaultSheetName, firstTeacherCellIndex)
+			if firstTeacherCellValue != "" {
 				k := j
-				for k < timeRowNum+2 {
-					kTeacherCell := sheet.Cell(k, 2*i+1)
-					if kTeacherCell.Value != firstTeacherCell.Value {
+				for k <= timeRowNum+2 {
+					kTeacherCellValue, _ := file.GetCellValue(ExportFileDefaultSheetName, fmt.Sprintf("%s%d", ReservationRoomColumn[i+1][0], k))
+					if kTeacherCellValue != firstTeacherCellValue {
 						break
 					}
 					k++
 				}
-				reservation, ok := rId2ReservationMap[firstTeacherCell.Value]
+
+				reservation, ok := rId2ReservationMap[firstTeacherCellValue]
 				if !ok {
-					log.Errorf("fail to find reservation %d", firstTeacherCell.Value)
+					log.Errorf("fail to find reservation %d", firstTeacherCellValue)
 					continue
 				}
-				firstStudentCell := sheet.Cell(j, 2*i+2)
-				firstTeacherCell.Merge(0, k-j-1)
-				firstTeacherCell.SetValue(textTopStyle)
-				firstTeacherCell.SetValue(fmt.Sprintf("%s\n%s\n%s",
-					reservation.TeacherFullname, reservation.TeacherMobile, reservation.TeacherAddress))
-				firstStudentCell.Merge(0, k-j-1)
-				firstStudentCell.SetValue(textTopStyle)
-				firstStudentCell.SetValue(fmt.Sprintf("%s\n%s",
-					reservation.StudentInfo.Fullname, reservation.StudentInfo.Mobile))
+
+				firstStudentCellIndex := fmt.Sprintf("%s%d", ReservationRoomColumn[i+1][1], j)
+				file.SetCellValue(ExportFileDefaultSheetName, firstTeacherCellIndex,
+					fmt.Sprintf("%s %s %s", reservation.TeacherFullname, reservation.TeacherMobile, reservation.TeacherAddress))
+				file.SetCellStyle(ExportFileDefaultSheetName, firstTeacherCellIndex, firstStudentCellIndex, textLeftTopStyle)
+				file.SetCellValue(ExportFileDefaultSheetName, firstStudentCellIndex,
+					fmt.Sprintf("%s %s", reservation.StudentInfo.Fullname, reservation.StudentInfo.Mobile))
+				file.SetCellStyle(ExportFileDefaultSheetName, firstStudentCellIndex, firstStudentCellIndex, textLeftTopStyle)
+				for mergeRow := j + 1; mergeRow < k; mergeRow++ {
+					mergeTeacherCellIndex := fmt.Sprintf("%s%d", ReservationRoomColumn[i+1][0], mergeRow)
+					mergeStudentCellIndex := fmt.Sprintf("%s%d", ReservationRoomColumn[i+1][1], mergeRow)
+					file.MergeCell(ExportFileDefaultSheetName, firstTeacherCellIndex, mergeTeacherCellIndex)
+					file.SetCellStyle(ExportFileDefaultSheetName, firstTeacherCellIndex, mergeTeacherCellIndex, textLeftTopStyle)
+					file.MergeCell(ExportFileDefaultSheetName, firstStudentCellIndex, mergeStudentCellIndex)
+					file.SetCellStyle(ExportFileDefaultSheetName, firstStudentCellIndex, mergeStudentCellIndex, textLeftTopStyle)
+				}
 				j = k
 			} else {
 				j++
 			}
 		}
 	}
-	// 调整列宽
-	sheet.SetColWidth(0, 0, 12)
-	sheet.SetColWidth(1, 2*len(arrangement.Rooms), 20)
 
-	err = file.Save(path)
+	file.SetActiveSheet(sheetIndex)
+	err = file.SaveAs(path)
 	if err != nil {
 		return re.NewRError("fail to save file to path", err)
 	}
--- buslogic/verification.go
+++ buslogic/verification.go
@@ -1,10 +1,5 @@
 package buslogic
 
-import (
-	"github.com/shudiwsh2009/reservation_thxx_go/utils"
-	"github.com/tealeg/xlsx"
-)
-
 type StudentVerification struct {
 	username string
 	fullname string
@@ -14,27 +9,28 @@ type StudentVerification struct {
 var username2StudentVerification = make(map[string]*StudentVerification)
 
 func (w *Workflow) loadStudentVerification(filename string) error {
-	file, err := xlsx.OpenFile(filename)
-	if err != nil {
-		return err
-	}
-	sheet := file.Sheets[0]
-	for _, row := range sheet.Rows {
-		if len(row.Cells) != 3 {
-			break
-		}
-		username := row.Cells[0].Value
-		if !utils.IsStudentUsername(username) {
-			continue
-		}
-		fullname := row.Cells[1].Value
-		verified := row.Cells[2].Value
-		username2StudentVerification[username] = &StudentVerification{
-			username: username,
-			fullname: fullname,
-			verified: verified == "是",
-		}
-	}
+	// TODO(shudi) need repair
+	//file, err := xlsx.OpenFile(filename)
+	//if err != nil {
+	//	return err
+	//}
+	//sheet := file.Sheets[0]
+	//for _, row := range sheet.Rows {
+	//	if len(row.Cells) != 3 {
+	//		break
+	//	}
+	//	username := row.Cells[0].Value
+	//	if !utils.IsStudentUsername(username) {
+	//		continue
+	//	}
+	//	fullname := row.Cells[1].Value
+	//	verified := row.Cells[2].Value
+	//	username2StudentVerification[username] = &StudentVerification{
+	//		username: username,
+	//		fullname: fullname,
+	//		verified: verified == "是",
+	//	}
+	//}
 	return nil
 }
 
